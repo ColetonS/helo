fe6b7870c32f429050c8411562a4566f26b464b2
ColetonS <coleton.m.smith@gmail.com>
2019-08-09 12:29:30 -0600

    login controller setup

--- server/controller.js
+++ server/controller.js
@@ -20,5 +20,21 @@ module.exports = {
         .catch(err => {
             res.status(500).send({message: 'Failed to register'})
         })
+    },
+    login: async (req, res) => {
+        const db = req.app.get('db')
+        const {username, password} = req.body
+        const user = await db.find_username([username])
+        if (user.length === 0) {
+            return res.status(400).send({message: 'User not found'})
+        }
+        if (password === user[0].password) {
+            req.session.user = user[0]
+            return res.status(200).send({
+            message: 'Logged in',
+            user: req.session.user, 
+            loggedIn: true})
+        }
+        
     }
 }
